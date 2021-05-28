09d352d5ce5a90fb819abf5b25d10f0413ba83ba
Bantita Boonyadate <bantita_boonyadate@cmu.ac.th>
2021-05-28 16:18:02 +0700

    commit4 add alert
    add alert

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var newText = 'new Text'
-document.getElementById('ok').innerHTML = newText
-var nameMe = 'Bantita'
-document.getElementById('no').innerHTML = nameMe
+document.getElementById('ok').addEventListener('click', function(e) {
+    alert('ok is clicked')
+}
+)
