de2a5bcb0391c2cab21ab7cd3704c8e706ee4902
Bantita Boonyadate <bantita_boonyadate@cmu.ac.th>
2021-05-28 16:59:43 +0700

    commit6 add alert 3 button
    add alert

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.getElementById('ok').addEventListener('click', function(e) {
-    alert('ok is clicked')
-    console.log(e)
+    alert('This button is ok')
 }
 )
 document.getElementById('cancel').addEventListener('mousemove', function(c) {
     var x = c.clientX;
     var y = c.clientY;
 	console.log('x: ' +x+ ' ,'+ ' y: ' +y);
+}
+)
+document.getElementById('cancel').addEventListener('click', function(e) {
+    alert('This button is cancel')
+}
+)
+document.getElementById('no').addEventListener('click', function(e) {
+    alert('This button is no')
 }
 )
