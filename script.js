0740411490cd06d8d796d2d75e76de9de853f7d0
Bantita Boonyadate <bantita_boonyadate@cmu.ac.th>
2021-05-28 16:51:28 +0700

    commit5 add mouse
    add mousemove

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.getElementById('ok').addEventListener('click', function(e) {
     alert('ok is clicked')
-    
+    console.log(e)
+}
+)
+document.getElementById('cancel').addEventListener('mousemove', function(c) {
+    var x = c.clientX;
+    var y = c.clientY;
+	console.log('x: ' +x+ ' ,'+ ' y: ' +y);
 }
 )
